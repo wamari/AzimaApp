bab416745e67d1932d4fcf704873118dcbc56d0d
Eric Wamari <wamari@harlertechnologies.com>
2017-08-15 01:25:34 +0300

    tweaks

--- app/src/main/java/com/harlertechnologies/azima/RegisterActivity.java
+++ app/src/main/java/com/harlertechnologies/azima/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.harlertechnologies.azima;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void onBackPressed(View view){
+        super.onBackPressed();
     }
 }
